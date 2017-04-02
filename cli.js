7be4965d8304e83dae01100d432f9d9af15487a0
Dung Nguyen <dungntnew@gmail.com>
2017-04-02 14:31:09 +0900

    impl cli

--- cli.js
+++ cli.js
@@ -3,6 +3,10 @@ const _ = require('lodash');
 const fs = require('fs');
 const prompt = require('syncprompt');
 const colors = require('colors');
+const Table = require('cli-table');
+const util  = require('util');
+const request = require('sync-request');
+
 const _pr = console.log;
 const debug = console.debug;
 
@@ -21,9 +25,156 @@ colors.setTheme({
 });
 
 const Config = {
-    SS_FN: __dirname + '/session_.json',
-    QUIZ_DIR: __dirname + '/quest',
-    EMO_FN: __dirname + '/emotion.json',
+    SS_FN: __dirname + '/.session.json',
+    QUIZ_DIR: __dirname + '/data/quest',
+    EMO_FN: __dirname + '/data/emotion.json',
+    JVD_FN: __dirname + '/data/japanese_vietnamese.json',
+    KJD_FN: __dirname + '/data/kanji.json',
+    LIMIT: 5,
+    GG_URL: 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&dt=bd&dj=1&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=at&sl=%s&tl=%s&q=%s',
+}
+
+class Dict extends Object {
+
+    constructor() {
+        super();
+    
+        this.wdb = JSON.parse(fs.readFileSync(Config.JVD_FN, 'utf8'));
+        this.kdb = JSON.parse(fs.readFileSync(Config.KJD_FN, 'utf8'));
+    }
+
+    isKanji(str) {
+        return /^[\u4e00-\u9faf]+$/.test(str);
+    }
+    
+    isJapanese(str) {
+        return /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/.test(str);
+    }
+
+    format(content) {
+        if (_.isEmpty(content)) return '';
+        
+        if (!_.includes(content, '◆')) {
+            return content;
+        }
+        return content.split('◆').join('\n\n◆')
+                      .split('※').join('\n\n※')
+                      .split(':').join('\n')
+    }
+
+    summary(content) {
+        if (_.isEmpty(content)) return '';
+        
+        if (!_.includes(content, '◆')) {
+            return content;
+        }
+
+        const lines = content.split('◆')
+        const phontic = _.trim(lines[0].replace('∴「', '').replace(/」.*/, ''));
+        
+        const means = []
+        for (let i = 1; i < lines.length - 1; i++) {
+            const parts = lines[i].split('※');
+            if (parts.length > 1) {
+                means.push(_.trim(parts[0]));
+            }
+        }
+        return {
+            phontic: phontic,
+            means: means
+        }
+    }
+
+    searchWord(word) {
+        let results = _.uniqBy(_.filter(this.wdb, (w) => {
+            return w.word === word;
+        }), r => r.content);
+ 
+        results = _.slice(results, 0, Config.LIMIT);
+
+       let others = []
+        if (results.length < Config.LIMIT) {
+            let others = _.unionBy(_.filter(this.wdb, (w) => {
+                return w.word !== word 
+                       &&
+                       _.startsWith(w.word, word); 
+            }), r => r.content);
+            others = _.slice(others, 0, Config.LIMIT - results.length);
+        }
+
+        return {
+            found: results.length > 0,
+            matches: _.map(results, r => ({
+                id: r.id,
+                word: r.word,
+                summary: this.summary(r.content),
+                content: this.format(r.content),
+                kanjiInfo: this.briefKanji(word + r.word),
+            })),
+            others: _.map(others, r => ({
+                id: r.id,
+                word: r.word,
+                kanjiInfo: this.briefKanji(word + r.word),
+                summary: this.summary(r.content),
+                content: this.format(r.content),
+            })),
+        }
+    }
+
+    searchKanji(kanji) {
+        let results = _.filter(this.kdb, (k) => {
+            return k.Word === kanji || _.toUpper(k.Mean) === _.toUpper(kanji)
+        });
+
+        return {
+            found: results.length > 0,
+            matches: results,
+        }
+    }
+
+    briefKanji(text) {
+    　　const kanjis = _.uniq(_.filter(text.split(''), (w) => {
+                return this.isKanji(w)
+        }));
+
+        return _.map(kanjis, k => {
+            const r = this.searchKanji(k);
+            if (!r.found) {
+                return `${k}-<empty>`
+            }
+            else {
+                const d = r.matches[0];
+                return `${k}-${d.Mean}`
+            }
+        }).join(' | ');
+    }
+
+    trans(text, sl, tl) {
+        let reply     = {};
+        const q         = encodeURIComponent(text);
+        if (!sl) {
+            sl = this.isJapanese(text) ? 'ja': 'vi';
+            tl = sl === 'ja' ? 'vi': 'ja';
+        }
+        const url       = util.format(Config.GG_URL, sl, tl, q);
+        
+        const res = request('GET', url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36',
+                'Content-Type': 'application/json; charset=utf-8',
+                'accept-language': 'en-US,en;q=0.8,en-GB;q=0.6,vi;q=0.4,ja;q=0.2,zh-CN;q=0.2,zh;q=0.2',
+                'authority': 'translate.googleapis.com'
+            }
+        })
+        
+        const raw = res.getBody('utf8');
+        const ret = JSON.parse(raw);
+        if (ret) {
+            ret.kanjiInfo =  this.briefKanji(raw);
+        }
+
+        return ret;
+    }
 }
 
 class QuizProvider extends Object {
@@ -182,6 +333,11 @@ class UI extends Object {
         return _.includes(['y', 'Y'], input);
     }
 
+    wait(msg='press any key to view more..[q for break]') {
+        this.i(msg)
+        return this.input() !== 'q';
+    }
+
     selectKey(options, msg, verbose=false) {
         this._();
         const func = () => {
@@ -237,6 +393,178 @@ class UI extends Object {
             }
         });
     }
+
+    table(head) {
+        return new Table({
+            chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+                    , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+                    , 'left': '' , 'left-mid': '-' , 'mid': '-' , 'mid-mid': '-'
+                    , 'right': '' , 'right-mid': '-' , 'middle': ' ' },
+            style: { 'padding-left': 0, 'padding-right': 0 },
+            head: head,
+        });
+    }
+
+    searchWordResults(word, result) {
+        
+        const table = this.table(['辞書検索', word])
+        const lines = [];
+
+        lines.push({'Found': result.found ? 'Yes[' + result.matches.length+']': 'No'});
+        
+        const each = (w) => {
+            let explain = (w.content || '-').replace(/##/g, '\n');
+            explain = this.splitter(explain, 80).join('\n');
+            
+            lines.push({'Word': w.word || '-'})
+            lines.push({'Phontic': w.summary.phontic ||'-'})
+            lines.push({'Kanji Info': this.limiter(w.kanjiInfo)});
+            lines.push({'Brief': w.summary.means.join(',') ||'-'})
+            lines.push({'Explain': explain})
+        }
+
+        _.each(result.matches, each); 
+
+        if (result.others.length > 0){
+            lines.push({'その他': result.others.length})
+            _.each(result.others, each); 
+        }
+
+        table.push(...lines);
+        this._();
+        console.info(table.toString());
+        this._();
+    };
+
+    splitter(str, l) {
+        var strs = [];
+        while(str.length > l){
+            var pos = str.substring(0, l).lastIndexOf(' ');
+            pos = pos <= 0 ? l : pos;
+            strs.push(str.substring(0, pos));
+            var i = str.indexOf(' ', pos)+1;
+            if(i < pos || i > pos+l)
+                i = pos;
+            str = str.substring(i);
+        }
+        strs.push(str);
+        return strs;
+    }
+
+    limiter(str, l=80) {
+        return this.splitter(str, l).join('\n');
+    };
+
+
+    searchKanjiResults(kanji, result) {
+        const head = this.table(['漢字検索', kanji])
+        head.push({'FOUND': result.found ? 'YES[' + result.matches.length+']': 'NO'});
+        this._();
+        console.info(head.toString());
+
+        for(let index=0; index< result.matches.length; index++) {
+            
+            const k = result.matches[index];
+            const components =_.map(k.ComponentDetails, c => {
+                return `${c.w || '-'}: ${c.h || '-'}`
+            }).join(' | ')
+
+            const examples = _.map(k.Examples, (e) => {
+                return {w: `${e.w}`, m: `${e.h} ${e.p} ${e.m}`};
+            });
+
+            let explain = (k.GiaiNghia || '-').replace(/##/g, '\n');
+            explain = this.limiter(explain);
+            
+            const body = this.table(['結果', `${index} / ${result.matches.length}`]);
+            const lines = [];
+            
+            lines.push({'Kanji': `${k.Word} 「 JLPT ${k.Level || '-'} 」` || '-'})
+            lines.push({'Freq': k.Freq ||'-'})
+            lines.push({'Stroke': k.StrokeCount ||'-'})
+            lines.push({'Mean': `${k.Mean} 「 ${components} 」`})
+            lines.push({'Onyomi': k.Onyomi ||'-'})
+            lines.push({'Kunyomi': k.Kunyomi ||'-'})
+            lines.push({'Explain': explain ||'-'})
+            
+            if (examples.length > 0) {
+                lines.push({'Examples': examples.length })
+            }
+            _.each(examples, ({w, m}, i) => {
+                lines.push({[w]: m});
+            });
+            body.push(...lines);
+            console.info(body.toString());
+
+            if (result.matches.length > 1 && index < result.matches.length - 1) {
+                if (!this.wait('press any key to view more..')) {
+                    break;
+                }
+            };
+
+            this._();
+        }
+         this._();
+    }
+
+    kanjiQuickView(kanji, result) {
+        const table = this.table(['漢字検索', kanji])
+        const lines = []
+        lines.push({'FOUND': result.found ? 'YES[' + result.matches.length+']': 'NO'});
+        
+        lines.push({'結果一覧': this.limiter(_.map(result.matches, k => k.Word).join(','), 40)});
+
+         const each = (k, index) => {
+            const components =_.map(k.ComponentDetails, c => {
+                return `${c.w || '-'}: ${c.h || '-'}`
+            }).join(' | ')
+            lines.push({'Kanji': `${k.Word} 「 JLPT ${k.Level || '-'} 」` || '-'})
+            lines.push({'Mean': `${k.Mean} 「 ${components} 」`})
+            lines.push({'Onyomi': k.Onyomi ||'-'})
+            lines.push({'Kunyomi': k.Kunyomi ||'-'})
+        }
+        _.each(result.matches, each); 
+        this._();
+        table.push(...lines);
+        console.info(table.toString());
+    }
+
+    transResult(word, result) {
+       
+        const table = this.table(['翻訳', word])
+        const lines = []
+        
+        if (!result) {
+            lines.push({'Trans': '<Empty>'})
+            lines.push({'Translit': '<Empty>'});
+        }          
+
+        const {sentences} = result;
+        
+        const trans = _.filter(_.map(sentences, s => s.trans), (s) => {
+            return _.isString(s) && s.length > 0 
+        });
+
+        const translit = _.find(_.map(sentences, s => {
+            return result.src === 'vi' 
+                        ? s.translit
+                        : s.src_translit;
+        }), (s) => {
+            return _.isString(s) && s.length > 0 
+        });
+
+        _.each(trans, (t) => {
+            lines.push({ 'Trans': this.limiter(t || '-')});
+        })
+        
+        lines.push({'Translit': this.limiter(translit || '-')});
+        lines.push({'Kanji Info': this.limiter(result.kanjiInfo)});
+        table.push(...lines);
+        this._();
+        console.info(table.toString()); 
+    }
+    
+    
 
     endQuiz(playing) {
         this._();
@@ -308,7 +636,7 @@ class Session extends Object {
         
         const {info} = lession;
         let newPlaying
-        if (playing.key === info.key) {
+        if (playing.key === info.key && playing.curr < playing.total) {
              newPlaying = _.merge({}, info, playing);
         }
         else {
@@ -350,6 +678,8 @@ class CLI extends Object {
         
         this.name = 'CLI';
         this.session = new Session();
+        this.dict = new Dict();
+        
         this.ui = new UI();
 
         this.handlers = {
@@ -359,22 +689,25 @@ class CLI extends Object {
             home: this.quizMenu.bind(this),
             m: this.quizMenu.bind(this),
 
-            dict: this.dict.bind(this),
-            d: this.dict.bind(this),
-            t: this.dict.bind(this),
+            dict: this.search.bind(this),
+            d: this.search.bind(this),
+            t: this.trans.bind(this),
             
             kanji: this.kanji.bind(this),
-            look: this.kanji.bind(this),
+            quick: this.quick.bind(this),
             info: this.kanji.bind(this),
             k: this.kanji.bind(this),
             
             quit: this.quit.bind(this),
             exit: this.quit.bind(this),
+            q: this.quit.bind(this),
             
             help: this.help.bind(this),
             h: this.help.bind(this),
-            _: this.help.bind(this),
+            _: this.smart.bind(this),
         }
+
+        this.lastInput = '';
     };
 
     help(handlers) {
@@ -390,17 +723,19 @@ class CLI extends Object {
             const raw = this.ui.input();
             const strs = raw.split(' ');
             const cmd = strs[0];
-            const args = _.slice(strs, 1); 
+            const args = _.slice(strs, 1).join(' '); 
 
             const handler = handlers[cmd];
             
             let complete 
             
             if (handler) {
-                complete = handler(...args);
+                this.lastInput = args;
+                complete = handler(args);
             }
             
             else if (handlers._) {
+                this.lastInput = raw;
                 complete = handlers._(raw);
             }
             
@@ -471,12 +806,66 @@ class CLI extends Object {
         return this.gameStart(lessionInfo)
     }
 
-    dict(word) {
-        this.ui.msg('translate for: ' + word);
+    smart(input) {
+        if (input && _.isString(input)) {
+            if (input.length > 4) {
+                this.trans(input);
+            }
+            else if (input.length == 1) {
+                this.kanji(input);
+            }
+            else {
+                this.search(input);
+            }
+        }
     }
 
-    kanji(kanji) {
-        this.ui.msg('lookup for kanji: ' + kanji);
+    search(word) {
+        if (_.isEmpty(_.trim(word))) {
+            return;
+        }
+        const r = this.dict.searchWord(word);
+        this.ui.searchWordResults(word, r);
+    }
+
+    trans(word) {
+        const r = this.dict.trans(word);
+        this.ui.transResult(word, r);
+    }
+
+    kanji(word) {
+        if (_.isEmpty(_.trim(word))) {
+            return;
+        }
+
+        const kanjis = this.dict.isKanji(word) ? word.split(''): [word];
+        for(let i = 0; i < kanjis.length; i++) {
+            const k = kanjis[i];
+            const r = this.dict.searchKanji(k);
+            this.ui.searchKanjiResults(k, r);
+
+            if (kanjis.length > 1 && i < kanjis.length - 1)  {
+                if (!this.ui.wait()) {
+                    break;
+                }
+            }
+        }
+    }
+
+    quick(word) {
+        word = this.infer(word);
+        const kanjis = this.dict.isKanji(word) ? word.split(''): [word];
+        for(let i = 0; i < kanjis.length; i++) {
+            const k = kanjis[i];
+            const r = this.dict.searchKanji(k);
+            this.ui.kanjiQuickView(k, r);
+
+            if (kanjis.length > 1 && i < kanjis.length - 1)  {
+                if (!this.ui.wait()) {
+                    break;
+                }
+            }
+        }
     }
 
     gameStart(lessionInfo) {
@@ -495,7 +884,49 @@ class CLI extends Object {
            
            // handle help
            h: (input) => {
+               if (this.quest) {
+                   this.ui.quizR(this.quest, playing.curr, playing.total);
+               } 
+           },
+           t: (input) => {
+               if (this.quest) {
+                   this.trans(this.quest.question);
+               }
+           },
 
+           v: (input) => {
+               if (this.quest) {
+                   const {question} = this.quest
+                   const {answer} = this.quest
+
+                   if (this.dict.isKanji(question)) {
+                       this.smart(question);
+                   }
+                   else {
+                       const answers = answer.split('※');
+                       _.each(answers, (a) => {
+                           this.search(a);
+                       });
+                   }
+               } 
+           },
+
+           k: (input) => {
+               if (this.quest) {
+                   const {question} = this.quest
+                   const {answer} = this.quest
+
+                   const text = (question + answer).replace('※', '');
+                　　const kanjis = _.uniq(_.filter(text.split(''), (w) => {
+                        return this.dict.isKanji(w)
+                    }));
+                    if (kanjis.length > 0) {
+                        this.kanji(kanjis.join(''));
+                    }
+                    else {
+                        this.ui.warn('No Kanji.');
+                    }
+               }  
            },
 
            // handle next q
@@ -518,10 +949,11 @@ class CLI extends Object {
             _: (input) => {
                 
                 const q = quest();
+                const a = q.answer.split('※');
                 const k = _.parseInt(input);
                 const c = _.parseInt(q.correct);
                 
-                if (k === c) {
+                if (k === c || input === a[c]) {
                     this.ui.quizR(q, playing.curr, playing.total);
                     this.ui.sugoi(playing.curr, playing.total);
                     playing.curr += 1;
@@ -533,7 +965,10 @@ class CLI extends Object {
             }
         });
 
-        const quest = () => lession.data[playing.curr];
+        const quest = () => {
+            this.quest = lession.data[playing.curr];
+            return this.quest;
+        }
 
         this.loop(handlers, ()=>  q || playing.curr >= playing.total, () => {
             this.ui.quizQ(quest(), playing.curr, playing.total);
